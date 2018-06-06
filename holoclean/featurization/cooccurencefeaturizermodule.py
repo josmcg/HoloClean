cfeeb8b58a146c7bdc0866fd97952f7d67a6029f
gmichalo <gmichalo@scspcxxx>
2018-06-06 15:41:28 -0400

    module featurizers

---
+++ holoclean/featurization/cooccurencefeaturizermodule.py
@@ -0,0 +1,139 @@
+import torch.nn as nn
+import torch
+from holoclean.global_variables import GlobalVariables
+
+
+class CooccurFeaturizer(nn.Module):
+
+    def __init__(self, n, l, f=1, update_flag=False, session= None):
+        """
+        Creates a pytorch module which will be a featurizer for HoloClean
+        :param n : number of random variables
+        :param l: number of classes
+        :param f: number of features in the group
+        :param update_flag: True if the values in tensor of the featurizer
+        need be updated
+
+        """
+        super(CooccurFeaturizer, self).__init__()
+        self.session = session
+        self.update_flag = update_flag
+        self.N = n
+        self.L = l
+        self.id = "SignalCooccur"
+        self.offset = self.session.feature_count
+        self.index_name = GlobalVariables.index_name
+        self.all_attr = list(self.session.init_dataset.schema.names)
+        self.all_attr.remove(self.index_name)
+
+        self.count = 0
+        self.pruning_object = self.session.pruning
+        self.domain_pair_stats = self.pruning_object.domain_pair_stats
+        self.dirty_cells_attributes = \
+            self.pruning_object.dirty_cells_attributes
+        self.domain_stats = self.pruning_object.domain_stats
+        self.threshold = self.pruning_object.threshold1
+        self.direct_insert = True
+
+        self.get_feature_id_map(1)
+
+
+
+
+        self.M = self.count
+        self.tensor = None
+
+    def forward(self):
+        """
+        Forward step of the featurizer
+        Creates the tensor for this specific feature
+        """
+        if self.tensor is None:
+            self.create_tensor()
+        else:
+            if self.update_flag:
+                #if the weights are updated we need to create again the tensor
+                self.create_tensor()
+
+        return self.tensor
+
+    def create_tensor(self, clean=1):
+        """
+        This method creates the tensor for the feature
+        """
+        tensor = torch.zeros(self.N, self.M, self.L)
+
+
+
+        domain_pair_stats = self.pruning_object.domain_pair_stats
+        domain_stats = self.pruning_object.domain_stats
+        cell_domain = self.pruning_object.cell_domain
+        cell_values = self.pruning_object.cellvalues
+
+        if clean:
+            vid_list = self.pruning_object.v_id_clean_list
+        else:
+            vid_list = self.pruning_object.v_id_dk_list
+
+        for vid in range(len(vid_list)):
+            for cell_index in cell_values[vid_list[vid][0] - 1]:
+
+                co_attribute = \
+                    cell_values[vid_list[vid][0] - 1][cell_index].columnname
+                attribute = vid_list[vid][1]
+                feature = self.attribute_feature_id.get(co_attribute, -1)
+
+                if co_attribute != attribute and feature != -1:
+                    domain_id = 0
+                    co_value = \
+                        cell_values[vid_list[vid][0] - 1][cell_index].value
+
+                    for value in cell_domain[vid_list[vid][2]]:
+                        v_count = domain_stats[co_attribute][co_value]
+                        count = domain_pair_stats[co_attribute][attribute].get(
+                            (co_value, value), 0)
+                        probability = count / v_count
+                        tensor[vid, feature - 1, domain_id] = probability
+                        domain_id = domain_id + 1
+        self.tensor = tensor
+
+        return self.tensor
+
+    def get_feature_id_map(self, clean=1):
+        """
+        Adding co-occur feature
+
+        :param clean: shows if create the feature table for the clean or the dk
+         cells
+
+        :return list
+        """
+        if clean:
+            self.offset = self.session.feature_count
+            self.attribute_feature_id = {}
+            feature_id_list = []
+            for attribute in self.dirty_cells_attributes:
+                self.count += 1
+                self.attribute_feature_id[attribute] = self.count
+                feature_id_list.append(
+                    [self.count + self.offset, attribute, 'Cooccur',
+                     'Cooccur'])
+            feature_df = self.session.holo_env.spark_session.createDataFrame(
+                feature_id_list,
+                self.session.dataset.attributes['Feature_id_map']
+            )
+            self.dataengine.add_db_table(
+                'Feature_id_map',
+                feature_df,
+                self.session.dataset,
+                append=1
+            )
+            self.session.feature_count += self.count
+        return []
+
+
+
+
+
+
+
---
+++ holoclean/featurization/initfeaturizermodule.py
@@ -0,0 +1,112 @@
+import torch.nn as nn
+import torch
+
+class Featurizer(nn.Module):
+
+    def __init__(self, n, l, f=1, update_flag=False, session=None, clean=1):
+        """
+        Creates a pytorch module which will be a featurizer for HoloClean
+        :param n : number of random variables
+        :param l: number of classes
+        :param f: number of features in the group
+        :param update_flag: True if the values in tensor of the featurizer
+        need be updated
+
+        """
+        super(Featurizer, self).__init__()
+        self.update_flag = update_flag
+        self.N = n
+        self.L = l
+        self.M = f-1
+        self.tensor = None
+
+        self.sesssion = session
+        self.id = "SignalInit"
+        self.table_name = self.dataset.table_specific_name('Init')
+        self.clean = clean
+
+
+    def forward(self):
+        """
+        Forward step of the featurizer
+        Creates the tensor for this specific feature
+        """
+        if self.tensor is None:
+            self.create_tensor()
+        else:
+            if self.update_flag:
+                #if the weights are updated we need to create again the tensor
+                self.create_tensor()
+
+        return self.tensor
+
+    def create_tensor(self):
+        """
+        This method creates the tensor for the feature
+        """
+        self.execute_query(self.clean)
+        tensor = torch.zeros(self.N, self.M, self.L)
+
+        query = "SELECT * FROM " + self.table_name
+        feature_table = self.dataengine.query(query, 1).collect()
+        for factor in feature_table:
+            tensor[factor.vid - 1, factor.feature - 1,
+                   factor.assigned_val - 1] = factor['count']
+        self.tensor = tensor
+
+        return self.tensor
+
+
+    def execute_query(self, clean=1):
+        """
+        Creates a string for the query that it is used to create the Initial
+        Signal
+
+        :param clean: shows if create the feature table for the clean or
+        the don't know cells
+
+        :return a list of length 1 with a string with the query
+        for this feature
+        """
+        if clean:
+            self.offset = self.session.feature_count
+        count = self.offset + 1
+        if clean:
+            name = "Observed_Possible_values_clean"
+        else:
+            name = "Observed_Possible_values_dk"
+
+        query_for_featurization = " SELECT  \
+            init_flat.vid as vid, init_flat.domain_id AS assigned_val, \
+            '" + str(1) + "' AS feature, \
+            1 as count\
+            FROM """ + \
+            self.dataset.table_specific_name(name) + \
+            " AS init_flat WHERE vid IS NOT NULL"
+
+        # if clean add signal fo Feature_id_map
+        if clean:
+            self.session.feature_count += count
+
+            index = self.offset + count
+            list_domain_map = [[index, 'Init', 'Init', 'Init']]
+            df_domain_map = self.session.holo_env.spark_session.\
+                createDataFrame(list_domain_map,
+                                self.dataset.attributes['Feature_id_map'])
+            self.session.holo_env.dataengine.add_db_table(
+                'Feature_id_map', df_domain_map, self.dataset, append=1)
+
+        table_name = self.id + str(clean)
+        self.table_name = self.dataset.table_specific_name(table_name)
+        query_for_table = "CREATE TABLE " + self.table_name + \
+                                  "(vid INT, assigned_val INT," \
+                                  " feature INT ,count INT);"
+        self.dataengine.query(query_for_table)
+        self.dataengine.query(query_for_featurization)
+
+
+
+
+
+
+
